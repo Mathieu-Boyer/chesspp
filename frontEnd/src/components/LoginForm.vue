f8559aff37e2cbb3d5fd2e85462f8a3ab619d43a
EZ3C <mboyer980@gmail.com>
2025-07-23 11:41:27 +0200

    login + register page created next step is connectng them to the backend

---
+++ frontEnd/src/components/LoginForm.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Button from "primevue/button"
+import { Form } from '@primevue/forms';
+import { InputText } from "primevue";
+
+</script>
+
+<template>
+
+    <div class="container">
+        <Form class="form">
+            <InputText name="email" type="email" placeholder="email" />
+            <InputText name="password" type="password" placeholder="password"/>
+            <Button label="Sign in"/>
+        </Form>
+    </div>
+
+</template>
+
+<style scoped>
+    .container{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        padding: 3rem;
+        width: fit-content;
+        background-color: #312e2b;
+        border-radius: .5rem;
+    }
+    .form{
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        width: 20rem;
+    }
+
+    input {
+        border: #6a6a6a solid 1px;
+    }
+    
+    button {
+        margin-top: 1rem;
+    }
+</style>
---
+++ frontEnd/src/pages/register.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Chesspp from '../components/Chesspp.Vue';
+import LoginForm from '../components/LoginForm.vue';
+import RegisterForm from '../components/registerForm.vue';
+
+</script>
+
+<template>
+
+<div class="formPage">
+    <Chesspp />
+    <RegisterForm></RegisterForm>
+    <router-link to="/login" class="link">
+    i already have account
+    </router-link>
+</div>
+</template>
+
+<style scoped>
+    .formPage{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        flex-direction: column
+    }
+
+    .link{
+        margin-top: 1.5rem;
+        text-decoration: underline;
+        cursor: pointer;
+    }
+    .link:visited{
+        color: white;
+    }
+</style>
