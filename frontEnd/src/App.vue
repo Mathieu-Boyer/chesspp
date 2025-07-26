72f0820c66bdeabcf7b6c7da2d9b748e4357e7b6
EZ3C <mboyer980@gmail.com>
2025-07-26 07:24:46 +0200

    profile page exists now

--- frontEnd/src/App.vue
+++ frontEnd/src/App.vue
@@ -4,7 +4,9 @@ import { RouterView } from 'vue-router'
 
 </script>
 <template>
+<div class="navBar">
 
+</div>
 <Suspense>
     <template #default>
       <RouterView />
@@ -17,5 +19,17 @@ import { RouterView } from 'vue-router'
 </template>
 
 <style scoped>
+.navBar{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 2rem;
+  height: 100vh;
+  padding: 1rem;
+  background-color: rgb(22, 22, 22);
+  border-right: 1px solid rgb(43, 43, 43);
 
+  position: fixed;
+
+}
 </style>
